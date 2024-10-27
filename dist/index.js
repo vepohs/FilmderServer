895a2dd0480e4e814e0bd2748824f64c504da90b
Voiturier Matthias <159275303+La227136@users.noreply.github.com>
2024-10-27 11:05:40 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+const greet = (name) => {
+    return `Bonjour, ${name}!`;
+};
+const addition = (a, b) => {
+    return a + b;
+};
+console.log(greet('Monde'));
+let myNumber;
+console.log(addition(2, 3));
+myNumber = addition(2, 3);
+console.log(myNumber);
+//# sourceMappingURL=index.js.map
---
+++ dist/test.js
@@ -0,0 +1,13 @@
+"use strict";
+const test = (name) => {
+    return `Bonjour, ${name}!`;
+};
+const add = (a, b) => {
+    return a + b;
+};
+console.log(test('Monde'));
+let aa;
+console.log(addition(6, 6));
+aa = add(6, 6);
+console.log(aa);
+//# sourceMappingURL=test.js.map
