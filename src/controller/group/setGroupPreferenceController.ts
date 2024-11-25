dee15c87bc25d89ddbca383598f6c53b4bf6d819
Voiturier Matthias <159275303+La227136@users.noreply.github.com>
2024-11-25 20:33:36 +0100

    tt fonctionne et ca a tjrs fonctionné mdr fck le responsive mdrrr

--- src/controller/group/setGroupPreferenceController.ts
+++ src/controller/group/setGroupPreferenceController.ts
@@ -4,14 +4,19 @@ import {AuthenticatedRequest} from "../../interface/interface";
 
 const groupService = new GroupService();
 export const setGroupPreference = async (req: AuthenticatedRequest, res: Response) => {
-    await groupService.setGroupPreference(req.body.groupId, req.body.genreIds, req.body.providerIds);
+    console.log("req.body", req.body);
+    await groupService.setGroupPreference(req.body.groupId, req.body.genrePreferenceIds, req.body.providerPreferenceIds);
     res.status(200).json({message: 'Preferences updated'});
 }
 export const getGroupPreference = async (req: AuthenticatedRequest, res: Response) => {
-    console.log("req.query", req.query);
+    console.log("req.query", req.query.groupId);
     const  groupId  = req.query.groupId as string;
 
     const genrePreference = await groupService.getGroupGenrePreference(groupId);
     const providerPreference = await groupService.getGroupProviderPreference(groupId);
+    console.log("UUUUUUUUUUUUUUUUUUUUUI")
+    console.log(genrePreference)
+    console.log(providerPreference)
+
     res.status(200).json({genrePreference,providerPreference});
 }
