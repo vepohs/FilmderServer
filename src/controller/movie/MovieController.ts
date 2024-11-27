dd650207b28b9c0ec7f490d2d9faaf1b90636275
vepohs <la227630@student.helha>
2024-11-27 08:25:49 +0100

    test

--- src/controller/movie/MovieController.ts
+++ src/controller/movie/MovieController.ts
@@ -8,11 +8,7 @@ const movieService = new MovieServices();
 const swipeService = new SwipeService();
 export const getMovie = async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     try {
-        console.log('req.body.listExcluedIds');
-        console.log(req.body.listExcluedIds);
         const listTableau = await movieService.getMovies(req.user!, req.body.listExcluedIds);
-        console.log('listTableau');
-        console.log(listTableau.map((movie) => movie.id));
         res.status(200).json({movie: listTableau});
     } catch (error: any) {
         next(error)
