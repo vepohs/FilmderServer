097377ec127255fe46bcb1fa22cb361cafffd944
vepohs <la227630@student.helha>
2024-12-16 12:00:11 +0100

    vps

--- src/server.ts
+++ src/server.ts
@@ -14,15 +14,16 @@ app.use(cors({
     origin: 'https://filmder.fr',  // Remplacez par l'URL de votre client
     credentials: true  // Autoriser les cookies
 }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use((req, res, next) => {
     console.log(`Received ${req.method} request for ${req.url}`);
     console.log('Headers:', req.headers);
     console.log('Body:', req.body);
     next();
 });
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 AppDataSource.initialize()
