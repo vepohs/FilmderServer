b522a50f185b44c3eb8f290222619306b3f49df4
vepohs <la227630@student.helha>
2024-12-16 11:22:06 +0100

    vps

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.SERVEUR_PORT
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://185.172.57.167',  // Remplacez par l'URL de votre client
+    origin: 'https://filmder.fr',  // Remplacez par l'URL de votre client
     credentials: true  // Autoriser les cookies
 }));
 
